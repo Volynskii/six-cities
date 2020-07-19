4cf67179bfce415ce99c3279c2d73d61687bd467
Антон <antonvolynskii@gmail.com>
2020-07-19 20:28:45 +1000

    added PropTypes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
 import App from "./components/app.jsx";
+import PropTypes from 'prop-types';
+
+
 
 const init = () => {
   ReactDOM.render(<App />, document.querySelector(`#root`));
