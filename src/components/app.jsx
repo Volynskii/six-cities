475075e50345ffd1d54c9d5e57dbf7ee41325298
Антон <antonvolynskii@gmail.com>
2020-07-19 20:21:33 +1000

    created app,header,main components then connect them with index.js

---
+++ src/components/app.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Header from "./header.jsx";
+import Main from "./main.jsx";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Main />;
+    </>
+  );
+};
+export default App;
