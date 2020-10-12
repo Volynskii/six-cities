915914e8f3207d3f04d3199073c021075b50a07f
Антон <antonvolynskii@gmail.com>
2020-10-13 01:51:27 +1000

    update api

--- src/redux/auth/actions.js
+++ src/redux/auth/actions.js
@@ -2,6 +2,9 @@ import * as types from "./types";
 import {history} from "../index";
 import {parseAuthData} from "../../helpers/parse-auth-data";
 
+
+const BAD_REQUEST_STATUS = 400;
+
 export const loginAsync = (email, password) => (dispatch, _getState, api) => {
   return api
     .post(`/login`, {email, password})
@@ -12,30 +15,19 @@ export const loginAsync = (email, password) => (dispatch, _getState, api) => {
       }
     })
     .catch((error) => {
-      if (error.response.status) {
-        if (error.response.status === 400) {
-          dispatch(setAuthError(error.response.data.error));
-        }
+      if (error.response.status === BAD_REQUEST_STATUS) {
+        dispatch(setAuthError(error.response.data.error));
       }
     });
 };
 
 export const checkLoginAsync = () => {
   return (dispatch, _getState, api) => {
-    return api
-      .get(`/login`)
-      .then((response) => {
-        if (response.data) {
-          dispatch(login(response.data));
-        }
-      })
-      .catch((error) => {
-        if (error.response.status) {
-          if (error.response.status === 403) {
-            dispatch(setAuthError(error.response.data.error));
-          }
-        }
-      });
+    return api.get(`/login`).then((response) => {
+      if (response.data) {
+        dispatch(login(response.data));
+      }
+    });
   };
 };
 
